6e603f438456be6ce7a5efe1f5920adb22b8ac7e
Faizan Ahmad Siddiqui <faizan14sid@gmail.com>
2021-07-01 17:25:14 +0530

    create sidebar options

--- slack/src/Components/Sidebar.js
+++ slack/src/Components/Sidebar.js
@@ -4,6 +4,15 @@ import FibeManualReacordIcon from "@material-ui/icons/FiberManualRecord";
 import CreateIcon from "@material-ui/icons/Create";
 import { SidebarOption } from "./SidebarOption";
 import InsertCommentIcon from "@material-ui/icons/InsertComment";
+import InboxIcon from "@material-ui/icons/Inbox";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import BookmarkBoarderIcon from "@material-ui/icons/BookmarkBorder";
+import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
+import AppsIcon from "@material-ui/icons/Apps";
+import FileCopyIcon from "@material-ui/icons/FileCopy";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AddIcon from "@material-ui/icons/Add";
 
 export const Sidebar = () => {
   return (
@@ -19,7 +28,17 @@ export const Sidebar = () => {
         <CreateIcon />
       </div>
       <SidebarOption Icon={InsertCommentIcon} title="Threads" />
-      <SidebarOption title="community" />
+      <SidebarOption Icon={InboxIcon} title="Mentions & reaction" />
+      <SidebarOption Icon={DraftsIcon} title="Save items" />
+      <SidebarOption Icon={BookmarkBoarderIcon} title="Channel Browser" />
+      <SidebarOption Icon={PeopleAltIcon} title="People & user groups" />
+      <SidebarOption Icon={AppsIcon} title="Apps" />
+      <SidebarOption Icon={FileCopyIcon} title="File Browser" />
+      <SidebarOption Icon={ExpandLessIcon} title="Show less" />
+      <hr />
+      <SidebarOption Icon={ExpandMoreIcon} title="Channels" />
+      <hr />
+      <SidebarOption Icon={AddIcon} addChannelOption="" title="Add Channel" />
     </div>
   );
 };
